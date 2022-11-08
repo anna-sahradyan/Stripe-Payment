cee834379c0720dc99c9af9eff357850fc59d026
anna-sahradyan <a.sahradyan72@gmail.com>
2022-11-08 23:51:55 +0400

    start stripe project

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv")
+dotenv.config({path: "./.env"});
+const PORT = process.env.PORT || 4000
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-11-07",
+})
+app.use(express.static(process.env.STATIC_DIR));
+app.get("/", (req, res) => {
+    const path = resolve(process.env.STATIC_DIR + "/index.html");
+    res.sendFile(path);
+});
+app.get("/config", (req, res) => {
+    res.send({
+        publishableKey: process.env.STRIPE_SECRET_KEY,
+    });
+});
+app.post("/create-payment-intent", async (req, res) => {
+});
+app.listen(PORT,()=>
+console.log(`Node server listening at http://localhost:${PORT}`))
